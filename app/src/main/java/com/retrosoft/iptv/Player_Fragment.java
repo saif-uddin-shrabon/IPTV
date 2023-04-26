dc4ab058890c54f24c51092a1084c20974746276
saif-uddin-shrabon <saif06065@gmail.com>
2023-04-26 11:18:51 +0600

    change os push

--- app/src/main/java/com/retrosoft/iptv/Player_Fragment.java
+++ app/src/main/java/com/retrosoft/iptv/Player_Fragment.java
@@ -33,15 +33,6 @@ import timber.log.Timber;
 
 public class Player_Fragment extends Fragment {
 
-    private final String EXT_INF_SP = "#EXTINF:-1";
-    private final String KOD_IP_DROP_TYPE = "#KODIPROP:inputstream.adaptive.license_type=";
-    private final String KOD_IP_DROP_KEY = "#KODIPROP:inputstream.adaptive.license_key=";
-    private final String TVG_NAME = "tvg-name=";
-    private final String TVG_LOGO = "tvg-logo=";
-    private final String GROUP_TITLE = "group-title=";
-    private final String COMMA = ",";
-    private final String HTTP = "http://";
-    private final String HTTPS = "https://";
 
     EditText inputUrl,inputRfrl;
     Button btnPlay,btnDelete;
@@ -78,7 +69,11 @@ public class Player_Fragment extends Fragment {
                     public void onClick(View v) {
                         Url = inputUrl.getText().toString().trim();
                        String channelName = inputRfrl.getText().toString();
+
+                       // link and channel name store database
                         new dbmanagert(getContext()).addRecord(channelName,Url);
+
+                        // link extraction database
                         new GetChannelsTask(getContext()).execute(Url);
                     }
                 });
@@ -114,111 +109,5 @@ public class Player_Fragment extends Fragment {
 
         return view;
     }
-
-//    public void saveList() {
-//        String link = null;
-//        // Retrieve the link from the bundle
-//        Bundle bundle = getArguments();
-//        if (bundle != null) {
-//            link = bundle.getString("link");
-//        }
-//
-//        // Call the saveList() method
-//       new GetChannelsTask().execute(link);
-//
-//    }
-
-//    public class GetChannelsTask extends AsyncTask<String, Void, List<Map<String, String>>> {
-//
-//        @Override
-//        protected List<Map<String, String>> doInBackground(String... urls) {
-//            String urlString = urls[0];
-//            List<Map<String, String>> channels = new ArrayList<>();
-//            Map<String, String> currentChannel = new HashMap<>();
-//
-//            try {
-//                URL urlObj = new URL(urlString);
-//                HttpURLConnection conn = (HttpURLConnection) urlObj.openConnection();
-//                conn.setRequestMethod("GET");
-//                conn.connect();
-//
-//                BufferedReader reader = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-//                String line;
-//
-//                //new code
-//
-//                while ((line = reader.readLine()) != null) {
-//                    line = line.replaceAll("\"", "");
-//
-//                    if (line.startsWith(KOD_IP_DROP_TYPE)) {
-//                        currentChannel.put("channelDrmType", line.split(KOD_IP_DROP_TYPE)[1].trim());
-//
-//                    }else if (line.startsWith(KOD_IP_DROP_KEY)) {
-//                        currentChannel.put("channelDrmKey", line.split(KOD_IP_DROP_KEY)[1].trim());
-//
-//                    } else if (line.startsWith(EXT_INF_SP)) {
-//                        String[] parts = line.split(COMMA, 2);
-//                        if (parts.length > 1) {
-//                            String name = parts[1];
-//                            if (name.contains(TVG_NAME)) {
-//                                name = name.split(TVG_NAME)[1].split(TVG_LOGO)[0];
-//                            }
-//                            currentChannel.put("name", name);
-//                        }
-//
-//                        if (line.contains(GROUP_TITLE)) {
-//                            String groupTitle = line.split(GROUP_TITLE)[1].split(COMMA)[0];
-//                            currentChannel.put("channelGroup", groupTitle);
-//                        }
-//
-//
-//
-//                        if (line.contains(TVG_LOGO)) {
-//                            String logo = line.split(TVG_LOGO)[1].split(COMMA)[0];
-//                            currentChannel.put("logo", logo);
-//                        }
-//                    }
-//                    else if (line.startsWith(HTTP) || line.startsWith(HTTPS)) {
-//                        currentChannel.put("url", line);
-//                        channels.add(currentChannel);
-//                        currentChannel = new HashMap<>();
-//                    }
-//
-//
-//                }
-//
-//                reader.close();
-//                conn.disconnect();
-//
-//            } catch (IOException e) {
-//                Timber.e(e);
-//                Toast.makeText(getContext(), e.getLocalizedMessage(), Toast.LENGTH_SHORT).show();
-//            }
-//
-//            return channels;
-//        }
-//
-//        @Override
-//        protected void onPostExecute(List<Map<String, String>> channels) {
-//
-//            for (Map<String, String> channel : channels) {
-//                String name = channel.get("name");
-//                String channelGroup = channel.get("channelGroup");
-//                String logo = channel.get("logo");
-//                String url = channel.get("url");
-//                String channelDrmType = channel.get("channelDrmType");
-//                String channelDrmKey = channel.get("channelDrmKey");
-//
-//                new dbmanager(getContext()).addRecord(name,logo,url);
-//
-//     //           Log.d("Channel", "Name: " + name + ", Logo: " + logo + ", URL: " + url + ", channelGroup: " +channelGroup + ", channelDrmType: "+ channelDrmType + ", channelDrmKey: "+channelDrmKey);
-////
-////                Toast.makeText(getContext(), res, Toast.LENGTH_SHORT).show();
-//            }
-//
-//
-//
-//        }
-//    }
 
 }
--- app/src/main/java/com/retrosoft/iptv/listadapter.java
+++ app/src/main/java/com/retrosoft/iptv/listadapter.java
@@ -35,52 +35,6 @@ public class listadapter extends RecyclerView.Adapter<listadapter.myviewholder> 
     @Override
     public void onBindViewHolder(@NonNull listadapter.myviewholder holder, int position) {
 
-//        holder.Llink.setText(dataholder.get(position).getCOLUMN_LINK());
-//        holder.cName.setText(dataholder.get(position).getCOLUMN_NAME());
-//
-//
-//
-////        holder.viewLayout.setOnClickListener(new View.OnClickListener() {
-////            @Override
-////            public void onClick(View v) {
-//////                Context context = v.getContext();
-//////                Intent intent = new Intent(context, Player_Fragment.class);
-////                String link = dataholder.get(position).getCOLUMN_LINK();
-////                new Player_Fragment().saveList(link);
-//////                new Player_Fragment().new GetChannelsTask() {
-//////                    @Override
-//////                    protected void onPostExecute(List<Map<String, String>> channels) {
-//////                        // pass the link and channels data to the Player activity
-//////                        intent.putExtra("link", link);
-//////                       intent.putExtra("channels", (Serializable) channels);
-//////                        context.startActivity(intent);
-//////                    }
-//////                }.execute(link);
-////            }
-////        });
-//
-//        holder.viewLayout.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                String link = dataholder.get(position).getCOLUMN_LINK();
-//
-//                // Create a bundle to pass the link to the Player_Fragment
-//                Bundle bundle = new Bundle();
-//                bundle.putString("link", link);
-//
-// //                Create an instance of the Player_Fragment and set the bundle
-//                Player_Fragment playerFragment = new Player_Fragment();
-//                playerFragment.setArguments(bundle);
-//                playerFragment.saveList();
-//
-//                // Replace the current fragment with the Player_Fragment
-////                FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-////                transaction.replace(R.id.fragment_container, playerFragment);
-////                transaction.addToBackStack(null);
-////                transaction.commit();
-//            }
-//        });
-
 
             holder.Llink.setText(dataholder.get(position).getCOLUMN_LINK());
             holder.cName.setText(dataholder.get(position).getCOLUMN_NAME());
