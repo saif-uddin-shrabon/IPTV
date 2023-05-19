42de59679d4113d4ab774846b8815cbf868f02a9
saif-uddin-shrabon <saif06065@gmail.com>
2023-05-20 00:32:00 +0600

    update download link and other feature

--- app/src/main/java/com/retrosoft/iptv/Player_Fragment.java
+++ app/src/main/java/com/retrosoft/iptv/Player_Fragment.java
@@ -1,11 +1,14 @@
 package com.retrosoft.iptv;
 
+import static android.content.ContentValues.TAG;
+
 import android.app.Dialog;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.AsyncTask;
 import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -18,7 +21,18 @@ import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -37,7 +51,7 @@ public class Player_Fragment extends Fragment {
 
      public static  String channelName;
     EditText inputUrl,inputRfrl;
-    Button btnPlay;
+    Button btnPlay,btnDownloadLink;
     Dialog myDialog;
     FloatingActionButton floatingActionButton , downloadfloatingActionButton;
     RecyclerView recyclerView;
@@ -47,6 +61,7 @@ public class Player_Fragment extends Fragment {
     public static String triger = "";
     SearchView searchView;
     listadapter adapter;
+   public static boolean isAdd;
 
     @Override
     public void onResume() {
@@ -128,7 +143,7 @@ public class Player_Fragment extends Fragment {
             public void onClick(View v) {
                 Url = inputUrl.getText().toString().trim();
                 channelName = inputRfrl.getText().toString();
-
+                isAdd = true;
 
                 // link extraction database
                 new GetChannelsTask(getContext()).execute(Url);
@@ -145,7 +160,54 @@ public class Player_Fragment extends Fragment {
 
         myDialog = new Dialog(getActivity());
         myDialog.setContentView(R.layout.download_gateway);
+        btnDownloadLink = myDialog.findViewById(R.id.playerbtn);
 
+        btnDownloadLink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                isAdd = false;
+                String url = "https://algostack-bd.000webhostapp.com/apps/iptv.json";
+
+                // Create a JSON request using the provided URL.
+                JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, url, null,
+                        new Response.Listener<JSONObject>() {
+                            @Override
+                            public void onResponse(JSONObject response) {
+                                try {
+                                    // Extract the "data" array from the JSON object.
+                                    JSONArray dataArray = response.getJSONArray("data");
+
+                                    // Iterate through the JSON array and extract the desired fields.
+                                    for (int i = 0; i < dataArray.length(); i++) {
+                                        JSONObject item = dataArray.getJSONObject(i);
+                                        String name = item.getString("Name");
+                                        String url = item.getString("url");
+
+                                        new dbmanagert(getContext()).addRecord(name,url);
+                                        // Do whatever you want with the retrieved data.
+//                                        Log.d(TAG, "Name: " + name);
+//                                        Log.d(TAG, "URL: " + url);
+                                    }
+                                } catch (JSONException e) {
+                                    e.printStackTrace();
+                                }
+                            }
+                        },
+                        new Response.ErrorListener() {
+                            @Override
+                            public void onErrorResponse(VolleyError error) {
+                                // Handle error
+                                //Toast.makeText(getContext(), "Error retrieving data", Toast.LENGTH_SHORT).show();
+                                Log.e(TAG, "Error: " + error.getMessage());
+                            }
+                        });
+
+                RequestQueue requestQueue = Volley.newRequestQueue(getContext());
+
+                requestQueue.add(jsonObjectRequest);
+
+            }
+        });
         myDialog.show();
 
     }
